3edfb508aa13ab1dfaec19d91d1eb86d997528a3
Marcus Pearce <marcuspearce7@gmail.com>
2018-08-12 20:04:26 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// GITHUB DEMO
+
+var express = require("express");
+var app = express();
+
+app.set("view engine","ejs");
+
+
+// ROOT ROUTE
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+
+// SECOND ROUTE
+app.get("/second",function(req,res){
+    res.render("second");
+});
+
+
+// START SERVER
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("gitHubDemo has started");
+});
